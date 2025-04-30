d343b5ee6efc90eed602f2eec05ab3535730d8e4
nadyasyantikaa <nadiasantikanaraya@gmail.com>
2025-04-30 14:24:39 +0700

    first commit

---
+++ StackTugasMahasiswa21.java
@@ -0,0 +1,69 @@
+public class StackTugasMahasiswa21 {
+
+    Mahasiswa21[] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa21(int size) {
+        this.size = size;
+        stack = new Mahasiswa21[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        return top == size - 1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public void push(Mahasiswa21 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+
+    public Mahasiswa21 pop() {
+        if (!isEmpty()) {
+            Mahasiswa21 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai");
+            return null;
+        }
+    }
+
+    public Mahasiswa21 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
+    public Mahasiswa21 bottom() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
+    public int jumlahTugas() {
+        return top + 1;
+    }
+
+    public void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+}
